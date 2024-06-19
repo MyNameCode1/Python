f929406070347d6a8ea0658a97af20e08dac938e
MyNameCode1 <eminaa530@gmail.com>
2024-06-19 12:21:48 +0530

    Update 2. if_elif_else.py.py
    Error fixed

--- 2. if_elif_else.py.py
+++ 2. if_elif_else.py.py
@@ -7,7 +7,6 @@ If n is even and in the inclusive range of 2 to 5, print Not Weird
 If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than 20, print Not Weird
 """
-
 #!/bin/python3
 
 import math
@@ -25,8 +24,8 @@ if n % 2 != 0:
 elif n % 2 == 0 and (n > 2 and n < 5):
     print("Not Weird")
     
-elif n % 2 == 0 and (n > 6 and n < 20):
+elif n % 2 == 0 and (n > 6 and n <= 20):
     print("Weird")
     
-elif n % 2 == 0 and (n >= 20):
+elif n % 2 == 0 and (n > 20):
     print("Not Weird")
